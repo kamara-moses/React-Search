2ec7529b5a04d54570e7abf6609589dfa4581969
Moses Kamara <kamara.moses@yahoo.com>
2021-01-05 20:50:13 -0800

    made changes within the server.js file - deleted the app.use("express") code block

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use(express);
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World")
 })
 
 app.listen(PORT, () => {
