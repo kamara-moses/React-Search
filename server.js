f984c25148f41cab5681da5809762636e4e7db29
Moses Kamara <kamara.moses@yahoo.com>
2021-01-05 21:09:10 -0800

    established connection with mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,6 +9,10 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
+
+mongoose.connect("mongodb+srv://moseskamara:<password>@main.3fdxr.mongodb.net/<dbname>?retryWrites=true&w=majority", () => {
+    console.log("MongoDB Connection Established")
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
